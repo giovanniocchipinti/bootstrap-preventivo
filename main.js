48f4f5c8b3fa3537b0ba2bf6d2ecda0034393ea9
giovanniocchipinti <giovanniocchipinti2699@gmail.com>
2024-11-03 15:24:50 +0100

    feat: created funcion to calc price and created event on sumbit

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
 // variabili
-const workTime = 10;
-const workSelected = document.getElementById('workType').value 
-let workPrice = 0;
+const workTime = 10; //ore di lavoro
+const finalResult = document.getElementById('finalPrice');
+const result = finalResult.querySelector('#result')
+const calcForm = document.getElementById('prevForm');
+
+calcForm.addEventListener('submit', function (event) {
+    let workSelected = document.getElementById('workType').value; //tipo di lavoro
+    event.preventDefault();
+    result.innerHTML = priceCalc(workTime, workSelected);
+    finalResult.classList.remove('d-none')
+})
 
 
-// calcolo del prezzo in base al lavoro
-if (workSelected == 1) {
-    workPrice = 15.30;   
-}else if(workSelected == 2){
-    workPrice = 20.50;
-}else if(workSelected == 3){
-    workPrice = 33.60;
+
+//funzioni
+
+function priceCalc(workTime, workSelected) {
+    let workPrice = 0; // prezzo variabile in base al tipo di lavoro selezionato dall'utente
+    if (workSelected == 1) {
+        workPrice = 15.30;   
+    }else if(workSelected == 2){
+        workPrice = 20.50;
+    }else if(workSelected == 3){
+        workPrice = 33.60;
+    }
+    
+    let finalPrice = workPrice * workTime;
+   
+    
+
+    return finalPrice
 }
-
-
-
 
 
 
